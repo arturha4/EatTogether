b01e430b3e9b444b66f86491ccc2c8f4a1e67d98
arturha4 <sadbooys.2001@gmail.com>
2023-04-15 18:59:57 +0300

    hotfix

--- fridges/tasks.py
+++ fridges/tasks.py
@@ -10,10 +10,12 @@ def check_expired_products():
         current_date = current_datetime.date()
         if product.expiration_date == current_date:
             if not ProductExpiredNotification.objects.filter(user=product.user, text=f"Продукт {product.name} cкоро испортится. Срок годности до {product.expiration_date}"):
-                ProductExpiredNotification.objects.create(user=product.user, text=f"Продукт {product.name} скоро испортится. Срок годности до {product.expiration_date}")
+                notification = ProductExpiredNotification.objects.create(user=product.user, text=f"Продукт {product.name} скоро испортится. Срок годности до {product.expiration_date}")
+                notification.save()
         if product.expiration_date <= datetime.datetime.now().date():
             if not ProductExpiredNotification.objects.filter(user=product.user,
                                                              text=f"{product.name} испортился. Срок годности истекает {product.expiration_date}"):
-                ProductExpiredNotification.objects.create(user=product.user,
+                notification = ProductExpiredNotification.objects.create(user=product.user,
                                                           text=f"{product.name} испортился. Срок годности истеквет {product.expiration_date}")
+                notification.save()
     print('_________________________________________________________________________________________')
